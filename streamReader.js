b39da0a962cae829353336777eef892dd363a6bf
gonzalojpv <gonzalojpv@gmail.com>
2024-09-13 12:15:05 -0600

    GP, add salesforce-pubsub-api-client

--- streamReader.js
+++ streamReader.js
@@ -1,83 +1,45 @@
-const grpc = require("@grpc/grpc-js");
-const protoLoader = require("@grpc/proto-loader");
-const { authenticate } = require("./auth");
-const path = require("path");
+import PubSubApiClient from "salesforce-pubsub-api-client";
 
-const SALESFORCE_PUBSUB_API_URL = process.env.SALESFORCE_PUBSUB_API_URL;
-const TENANT_ID = process.env.TENANT_ID;
-// Path to your proto file (you need to download the relevant Pub/Sub proto files from Salesforce)
-const PROTO_PATH = path.join(__dirname, "/proto/pubsub.proto");
+async function run() {
+  try {
+    const client = new PubSubApiClient();
+    await client.connect();
 
-// Load the proto file
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
+    // Subscribe to account change events
+    const eventEmitter = await client.subscribe(
+      "/event/WorkOrderCreatedEvent__e"
+    );
 
-const pubsubProto = grpc.loadPackageDefinition(packageDefinition).eventbus.v1;
-
-// Function to subscribe to events
-async function subscribeToEvents(accessToken, instanceUrl) {
-  console.log("Subscribing to events...");
-
-  const client = new pubsubProto.PubSub(
-    SALESFORCE_PUBSUB_API_URL,
-    grpc.credentials.createSsl()
-  );
-
-  const metadata = new grpc.Metadata();
-  metadata.add("accesstoken", accessToken);
-  metadata.add("instanceurl", instanceUrl);
-  metadata.add("tenantid", TENANT_ID);
-
-  const subscriptionRequest = {
-    topic_name: "/event/ServiceAppointmentCreatedEvent__e",
-    num_requested: 10, // Number of events you want to receive at once
-  };
-
-  client.GetTopic(subscriptionRequest, metadata, (err, response) => {
-    console.log("err", err);
-    console.log("response", response);
-    if (err) {
-      console.error(
-        "Error subscribing to events:",
-        JSON.stringify(err, null, 2)
-      );
-      return;
-    }
-    const stream = client.Subscribe(metadata);
-
-    stream.on("data", (response) => {
-      console.log("Received event:", response);
-    });
-    stream.on("error", (err) => {
-      console.error(
-        "Error subscribing to events:",
-        JSON.stringify(err, null, 2)
+    // Handle incoming events
+    eventEmitter.on("data", (event) => {
+      if (event.payload && event.payload.ChangeEventHeader) {
+        console.log(
+          `Handling ${event.payload.ChangeEventHeader.entityName} change event ` +
+            `with ID ${event.replayId} ` +
+            `on channel ${eventEmitter.getTopicName()} ` +
+            `(${eventEmitter.getReceivedEventCount()}/${eventEmitter.getRequestedEventCount()} ` +
+            `events received so far)`
+        );
+      } else {
+        console.error(
+          "Failed to handle event: ChangeEventHeader is missing or invalid.",
+          event
+        );
+      }
+      // Safely log event as a JSON string
+      console.log(
+        JSON.stringify(
+          event,
+          (key, value) =>
+            /* Convert BigInt values into strings and keep other types unchanged */
+            typeof value === "bigint" ? value.toString() : value,
+          2
+        )
       );
     });
-
-    stream.on("end", () => {
-      console.log("Event stream ended.");
-    });
-
-    stream.write(subscriptionRequest);
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-// Authenticate and subscribe to Salesforce events
-authenticate()
-  .then(({ access_token, instance_url }) => {
-    try {
-      subscribeToEvents(access_token, instance_url);
-      console.log("Subscribed to events successfully.");
-    } catch (error) {
-      console.error("Error subscribing to events:", error);
-    }
-  })
-  .catch((error) => {
-    console.error("Authentication failed:", error);
-  });
+run();
